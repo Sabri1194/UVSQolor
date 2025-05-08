8913e7b785e08375da8a072236411c650957679e
Sabri1194 <immoula.sabri@gmail.com>
2025-05-08 14:32:22 +0200

    Sauvegarde des modifications locales contraste

--- src/uvsqolor.py
+++ src/uvsqolor.py
@@ -68,16 +68,17 @@ def correction_gamma(facteur):
     matrice = matrice_float.clip(0, max_value).astype(np.uint8)
     rafraichir()
 
-def correction_sigmoïde():
-    k=float(k)
+def correction_sigmoide(contraste,pente):
+    contraste=float(contraste)
+    pente= float(pente)
     matrice_float= matrice_2.astype(np.float32)
     max_value= float(np.iinfo(matrice.dtype).max)
     for i in range(matrice.shape[0]):
         for j in range(matrice.shape[1]):
             for c in range(3):
-            val_in= matrice_float[i,j,c]/max_value
-            val_out= 1/(1+np.exp(-k*val_in*(val_in-0.5)))
-
+                val_in= matrice_float[i,j,c]/max_value
+                val_out= 1/(1+np.exp(-pente*contraste*(val_in-0.5)))
+    rafraichir()
 
 # Callbacks
 def cb_ouvrir():
@@ -90,6 +91,7 @@ def cb_ouvrir():
         menu_effets.entryconfig('Vert',state=tk.NORMAL)
         menu_effets.entryconfig("Gris",state=tk.NORMAL)
         menu_effets.entryconfig("luminosité",state=tk.NORMAL)
+        menu_effets.entryconfig("Contraste",state= tk.NORMAL)
         rafraichir()
         
         
@@ -126,7 +128,38 @@ def cb_lumi():
     bouton_annuler.pack(side=tk.LEFT, padx=10)
 
 def cb_contraste():
-    """créer les slider de contraste(k)"""
+    """créer les slider de contraste"""
+    global dialogue_effet
+    dialogue_effet = tk.Toplevel(fenetre_principale)
+    dialogue_effet.title("Luminosité")
+    dialogue_effet.geometry("350x300")
+    dialogue_effet.grab_set()
+    slider1 = tk.Scale(dialogue_effet, from_=0.0, to=30.0,
+                      orient=tk.HORIZONTAL, length=200,
+                      resolution=0.1, digits=2,
+                      command=lambda x: correction_sigmoide(slider1.get(), slider2.get()))
+    slider1.set(1.0)
+    slider1.pack(pady=20)
+
+    slider2 = tk.Scale(dialogue_effet, from_=1, to=20,
+                      orient=tk.HORIZONTAL, length=200,
+                      resolution=0.1, digits=2,
+                      command=lambda x: correction_sigmoide(slider1.get(), slider2.get()))
+    slider2.set(1.0)
+    slider2.pack(pady=20)
+
+
+    frame_boutons = tk.Frame(dialogue_effet)
+    frame_boutons.pack(side=tk.BOTTOM, pady=10)
+
+    bouton_appliquer = tk.Button(frame_boutons, text="Appliquer",
+                                 command=applique_effet)
+    bouton_appliquer.pack(side=tk.LEFT, padx=10)
+
+    bouton_annuler = tk.Button(frame_boutons, text="Annuler",
+                               command=annule_effet)
+    bouton_annuler.pack(side=tk.LEFT, padx=10)
+
 
 # Création de la fenêtre principale
 fenetre_principale=tk.Tk()
@@ -146,5 +179,6 @@ menu_principal.add_cascade(label='Effets', menu=menu_effets)
 menu_effets.add_command(label='Vert',command=cb_vert,state=tk.DISABLED)
 menu_effets.add_command(label="Gris", command= cb_gris,state=tk.DISABLED)
 menu_effets.add_command(label="luminosité", command= cb_lumi,state= tk.DISABLED)
+menu_effets.add_command(label= "Contraste", command= cb_contraste,state=tk.DISABLED)
 
 fenetre_principale.mainloop()
