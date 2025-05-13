54051f6d05833fdff99cbf6aa2c6d47545ee424f
Sabri1194 <immoula.sabri@gmail.com>
2025-05-13 10:17:16 +0200

    Projet finalisé

--- src/uvsqolor.py
+++ src/uvsqolor.py
@@ -27,6 +27,7 @@ def rafraichir(ajouter_historique=True):
         historique.append(matrice.copy())
 
 
+
 def applique_effet():
     global matrice_2
     matrice_2 = matrice.copy()
@@ -48,6 +49,18 @@ def undo_effet():
     else:
         messagebox.showinfo("Info", "Aucun effet à annuler")
 
+def sauvegarder():
+    global matrice
+    chemin = filedialog.asksaveasfilename(
+    title="Enregistrer l'image",
+    defaultextension=".png",
+    filetypes=[("Images PNG", "*.png"), ("Toutes les fichiers", "*.*")])
+
+    if chemin:
+        image = Image.fromarray(matrice)
+        image.save(chemin)
+        messagebox.showinfo("Sauvegarde", "Image sauvegardée avec succès.")
+
 # Algorithmes de filtre
 def filtre_vert():
     global matrice
@@ -66,35 +79,30 @@ def filtre_gris():
 
 
 def correction_gamma(facteur):
-    global photo, matrice
-
-    gamma = 1 / float(facteur)
+    global matrice
+    facteur = 1 + float(facteur)
     matrice_float = matrice_2.astype(np.float32)
     max_value = float(np.iinfo(matrice.dtype).max)
-    for i in range(matrice.shape[0]):
-        for j in range(matrice.shape[1]):
-            for c in range(3):  # canal 0=R, 1=G, 2=B
-                val_in = matrice_float[i, j, c] / max_value
-                val_out = pow(val_in, float(gamma))
-                matrice_float[i, j, c] = max_value * val_out
 
-    matrice = matrice_float.clip(0, max_value).astype(np.uint8)
-    rafraichir(ajouter_historique=True)
+    matrice_float = matrice_float * facteur
+    matrice = np.clip(matrice_float, 0, max_value).astype(np.uint8)
+    rafraichir(ajouter_historique=False)
 
-def correction_sigmoide(c,k):
+def correction_contraste(facteur):
     global matrice, matrice_2
-    k=float(k)
-    c=float(c)
-    matrice_float= matrice_2.astype(np.float32)
-    max_value= float(np.iinfo(matrice.dtype).max)
+    facteur = float(facteur)  # facteur ∈ [-1, 1]
+    matrice_float = matrice_2.astype(np.float32)
+    max_value = float(np.iinfo(matrice.dtype).max)
+
     for i in range(matrice.shape[0]):
         for j in range(matrice.shape[1]):
-            for d in range(3):
-                val_in= matrice_float[i,j,d]/max_value
-                val_out= 1/(1+np.exp(-k*(val_in-0.5)*c))
-                matrice_float[i,j,d]=val_out*max_value
-    matrice = matrice_float.clip(0, max_value).astype(np.uint8)
-    rafraichir(ajouter_historique=True)
+            for c in range(3):
+                val_in = matrice_float[i, j, c] / max_value
+                val_out = 0.5 + (val_in - 0.5) * (1 + facteur * 2)  # linéaire autour de 0.5
+                matrice_float[i, j, c] = val_out * max_value
+
+    matrice = np.clip(matrice_float, 0, max_value).astype(np.uint8)
+    rafraichir(ajouter_historique=False)
 
 def convoleve2d(matrice, kernel, mode='same',boundary='symm'):
     for i in range(3):
@@ -110,6 +118,39 @@ def flou_gaussien(matrice,kernel,mode='same',boundary='symm'):
         image_flou = np.clip(image_flou, 0, 255).astype(np.uint8)
         matrice[:, :, i] = image_flou
                 
+
+def fusion(facteur,matrice_fusion):
+    global matrice
+    facteur= float(facteur)
+    if matrice.shape == matrice_fusion.shape:
+        matrice_float= matrice.astype(np.float32)
+        matrice_fusion_float= matrice_fusion.astype(np.float32)
+        max_value= float(np.iinfo(matrice.dtype).max)
+        for i in range(matrice.shape[0]):
+            for j in range(matrice.shape[1]):
+                for c in range(3):
+                    val_in_1= matrice[i,j,c]/max_value
+                    val_in_2= matrice_fusion[i,j,c]/max_value
+                    val_out= facteur*val_in_1+(1-facteur)*val_in_2
+                    matrice_float[i,j,c]=val_out*max_value
+        matrice = matrice_float.clip(0, max_value).astype(np.uint8)
+        matrice_2 = matrice.copy()
+        rafraichir(ajouter_historique=False)
+    else:
+        messagebox.showinfo("Erreur","Image de taille différente, fusion impossible")
+
+
+def filtre_detection_bords(matrice,kernel,mode="same",boundary="fill"):
+    for i in range(3):
+        image_2d = matrice[:, :, i]
+        image_flou = convolve2d(image_2d, kernel, mode=mode, boundary=boundary,fillvalue=0)
+        image_flou = np.clip(image_flou, 0, 255).astype(np.uint8)
+        matrice[:, :, i] = image_flou
+
+
+def negatif():
+    matrice[:,:,:] = 255- matrice[:,:,:]
+
 # Callbacks
 def cb_ouvrir():
     global nom_fichier, photo, canvas, matrice,matrice_2
@@ -124,7 +165,13 @@ def cb_ouvrir():
         menu_effets.entryconfig("Contraste",state=tk.NORMAL)
         menu_effets.entryconfig("Flou",state=tk.NORMAL)
         menu_effets.entryconfig("Flou Gaussien",state=tk.NORMAL)
-        menu_effets.entryconfig("Annuler",state=tk.NORMAL)
+        menu_effets.entryconfig("Fusion",state=tk.NORMAL)
+        menu_fichier.entryconfig('Sauvegarder',state=tk.NORMAL)
+        menu_effets.entryconfig('Détection de bords',state=tk.NORMAL)
+        menu_effets.entryconfig('Détection de bords Amélioré',state=tk.NORMAL)
+        menu_effets.entryconfig('Négatif',state=tk.NORMAL)
+
+
         historique.clear()
         historique.append(matrice.copy())
         rafraichir()
@@ -144,56 +191,44 @@ def cb_lumi():
     dialogue_effet.title("Luminosité")
     dialogue_effet.geometry("300x150")
     dialogue_effet.grab_set()
-    slider = tk.Scale(dialogue_effet, from_=0.1, to=3.0,
+
+    slider = tk.Scale(dialogue_effet, from_=-1.0, to=1.0,
                       orient=tk.HORIZONTAL, length=200,
-                      resolution=0.1, digits=2,
+                      resolution=0.05, digits=3,
                       command=correction_gamma)
-    slider.set(1.0)
+    slider.set(0.0)
     slider.pack(pady=20)
 
     frame_boutons = tk.Frame(dialogue_effet)
     frame_boutons.pack(side=tk.BOTTOM, pady=10)
 
-    bouton_appliquer = tk.Button(frame_boutons, text="Appliquer",
-                                 command=applique_effet)
+    bouton_appliquer = tk.Button(frame_boutons, text="Appliquer", command=applique_effet)
     bouton_appliquer.pack(side=tk.LEFT, padx=10)
 
-    bouton_annuler = tk.Button(frame_boutons, text="Annuler",
-                               command=annule_effet)
+    bouton_annuler = tk.Button(frame_boutons, text="Annuler", command=annule_effet)
     bouton_annuler.pack(side=tk.LEFT, padx=10)
 
 def cb_contraste():
-    """créer les slider de contraste et de pente"""
     global dialogue_effet
     dialogue_effet = tk.Toplevel(fenetre_principale)
     dialogue_effet.title("Contraste")
-    dialogue_effet.geometry("300x300")
+    dialogue_effet.geometry("300x150")
     dialogue_effet.grab_set()
-    slider1 = tk.Scale(dialogue_effet, from_=0.1, to=3.0,
+
+    slider = tk.Scale(dialogue_effet, from_=-1.0, to=1.0,
                       orient=tk.HORIZONTAL, length=200,
-                      resolution=0.1, digits=3,
-                      command=lambda x: correction_sigmoide(slider1.get(), slider2.get()))
-    slider1.set(1.0)
-    slider1.pack(pady=20)
-
-
-    slider2 = tk.Scale(dialogue_effet, from_=0.1, to=20.0,
-                      orient=tk.HORIZONTAL, length=200,
-                      resolution=1.0, digits=2,
-                      command=lambda x: correction_sigmoide(slider1.get(), slider2.get()))
-
-    slider2.set(10.0)
-    slider2.pack(pady=20)
+                      resolution=0.05, digits=3,
+                      command=correction_contraste)
+    slider.set(0.0)
+    slider.pack(pady=20)
 
     frame_boutons = tk.Frame(dialogue_effet)
     frame_boutons.pack(side=tk.BOTTOM, pady=10)
 
-    bouton_appliquer = tk.Button(frame_boutons, text="Appliquer",
-                                 command=applique_effet)
+    bouton_appliquer = tk.Button(frame_boutons, text="Appliquer", command=applique_effet)
     bouton_appliquer.pack(side=tk.LEFT, padx=10)
 
-    bouton_annuler = tk.Button(frame_boutons, text="Annuler",
-                               command=annule_effet)
+    bouton_annuler = tk.Button(frame_boutons, text="Annuler", command=annule_effet)
     bouton_annuler.pack(side=tk.LEFT, padx=10)
 
 
@@ -208,6 +243,54 @@ def cb_flou_gauss():
     flou_gaussien(matrice,kernel)
     rafraichir(ajouter_historique=True)
 
+
+def cb_fusion():
+    global dialogue_effet    
+    nom_fichier=filedialog.askopenfilename(title='Ouvrir une image')
+    if nom_fichier is not None:
+        img=Image.open(nom_fichier)
+        img = img.resize((matrice.shape[1], matrice.shape[0]))
+        matrice_fusion=np.array(img)
+        
+    
+    dialogue_effet = tk.Toplevel(fenetre_principale)
+    dialogue_effet.title("Fusion")
+    dialogue_effet.geometry("300x150")
+    dialogue_effet.grab_set()
+    slider = tk.Scale(dialogue_effet, from_=0.1, to=1.0,
+                      orient=tk.HORIZONTAL, length=200,
+                      resolution=0.1, digits=3,
+                      command=lambda val: fusion(float(val), matrice_fusion))
+    slider.set(0.5)
+    slider.pack(pady=20)
+
+    frame_boutons = tk.Frame(dialogue_effet)
+    frame_boutons.pack(side=tk.BOTTOM, pady=10)
+
+    bouton_appliquer = tk.Button(frame_boutons, text="Appliquer",
+                             command=dialogue_effet.destroy)
+    bouton_appliquer.pack(side=tk.LEFT, padx=10)
+
+    bouton_annuler = tk.Button(frame_boutons, text="Annuler",
+                               command=annule_effet)
+    bouton_annuler.pack(side=tk.LEFT, padx=10)
+
+
+def cb_detect():
+    kernel_laplace= np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
+    filtre_detection_bords(matrice,kernel_laplace)
+    rafraichir(ajouter_historique=True)
+
+def cb_detect_amel():
+    kernel_laplace= np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
+    filtre_gris()
+    filtre_detection_bords(matrice,kernel_laplace)
+    rafraichir(ajouter_historique=True)
+
+def cb_negatif():
+    negatif()
+    rafraichir(ajouter_historique=True)
+
 # Création de la fenêtre principale
 fenetre_principale=tk.Tk()
 fenetre_principale.title('UVSQolor')
@@ -220,6 +303,11 @@ fenetre_principale.config(menu=menu_principal)
 menu_fichier=tk.Menu(menu_principal,tearoff=0)
 menu_principal.add_cascade(label='Fichier', menu=menu_fichier)
 menu_fichier.add_command(label='Ouvrir',command=cb_ouvrir)
+menu_fichier.add_command(label='Sauvegarder',command=sauvegarder, state= tk.DISABLED)
+
+bouton_menu_annuler = tk.Menu(menu_principal, tearoff=0)
+menu_principal.add_command(label='Annuler', command=undo_effet)
+
 
 menu_effets=tk.Menu(menu_principal,tearoff=0)
 menu_principal.add_cascade(label='Effets', menu=menu_effets)
@@ -228,8 +316,11 @@ menu_effets.add_command(label="Gris", command= cb_gris,state=tk.DISABLED)
 menu_effets.add_command(label="luminosité", command= cb_lumi,state= tk.DISABLED)
 menu_effets.add_command(label="Contraste", command= cb_contraste,state= tk.DISABLED)
 menu_effets.add_command(label="Flou", command= cb_flou,state= tk.DISABLED)
-menu_effets.add_command(label="Flou Gaussien ",command=flou_gaussien,state=tk.DISABLED)
-menu_effets.add_command(label="Annuler",command=undo_effet,state=tk.DISABLED)
+menu_effets.add_command(label="Flou Gaussien",command=cb_flou_gauss,state=tk.DISABLED)
+menu_effets.add_command(label="Fusion",command=cb_fusion,state=tk.DISABLED)
+menu_effets.add_command(label="Détection de bords",command=cb_detect,state=tk.DISABLED)
+menu_effets.add_command(label="Détection de bords Amélioré",command=cb_detect_amel,state=tk.DISABLED)
+menu_effets.add_command(label="Négatif",command=cb_negatif,state=tk.DISABLED)
 
 
 fenetre_principale.mainloop()
